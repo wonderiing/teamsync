4a2bc16d86ec2c79c142d30d1d13995e48498d30
wndr <ca223871@gmail.com>
2025-09-22 21:06:53 -0600

    v1

--- ms-requests/src/main/java/com/tecmilenio/carlos/ms/requests/controllers/RequestController.java
+++ ms-requests/src/main/java/com/tecmilenio/carlos/ms/requests/controllers/RequestController.java
@@ -20,7 +20,7 @@ import java.net.URI;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/requests")
+@RequestMapping("/api/v1/requests")
 @Validated
 public class RequestController {
 
